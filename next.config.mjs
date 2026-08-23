9634201b788ee33fb5ea7f2f047d923bed82512f
EasinTanvir <easintanvir102@gmail.com>
2026-08-23 20:56:34 +0600

    remove config

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  serverExternalPackages: ["@huggingface/transformers", "onnxruntime-node"],
+  /* config options here */
 };
 
 export default nextConfig;
